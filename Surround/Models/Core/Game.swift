honganhkhoa/Surround

commit 816c6a1d67b41536c6c562388b3ca011261b874e

[Translation] Remove some redundant strings; bump app version.

Surround/Models/Core/Game.swift
@@ -669,9 +669,9 @@ class Game: ObservableObject, Identifiable, CustomDebugStringConvertible, Equata
             }
             let difference = whiteScore + (gameData?.komi ?? 0) - blackScore
             if difference > 0 {
-                return String(format: "White by %.1f", difference)
+                return String(localized: "White by \(difference, specifier: "%.1f")")
             } else {
-                return String(format: "Black by %.1f", -difference)
+                return String(localized: "Black by \(difference, specifier: "%.1f")")
             }
         } else {
             if gamePhase == .stoneRemoval {
@@ -691,7 +691,7 @@ class Game: ObservableObject, Identifiable, CustomDebugStringConvertible, Equata
                     } else {
                         let time = userStoneColor == .black ? clock?.blackTime : clock?.whiteTime
                         if let timeLeft = time?.timeLeft, timeLeft <= 10 {
-                            return String(localized: "Your move (\(String(format: "%02d", Int(timeLeft))))", comment: "Integer is for time left")
+                            return String(localized: "Your move (\(Int(timeLeft), specifier: "%02d"))", comment: "Integer is for time left")
                         }
                         return String(localized: "Your move", comment: "Game")
                     }

Surround.xcodeproj/project.pbxproj
@@ -1404,6 +1404,7 @@
 				CLANG_WARN_UNREACHABLE_CODE = YES;
 				CLANG_WARN__DUPLICATE_METHOD_MATCH = YES;
 				COPY_PHASE_STRIP = NO;
+				CURRENT_PROJECT_VERSION = 2024013101;
 				DEBUG_INFORMATION_FORMAT = dwarf;
 				ENABLE_STRICT_OBJC_MSGSEND = YES;
 				ENABLE_TESTABILITY = YES;
@@ -1422,6 +1423,7 @@
 				GCC_WARN_UNUSED_FUNCTION = YES;
 				GCC_WARN_UNUSED_VARIABLE = YES;
 				IPHONEOS_DEPLOYMENT_TARGET = 16.0;
+				MARKETING_VERSION = 1.3.12;
 				MTL_ENABLE_DEBUG_INFO = INCLUDE_SOURCE;
 				MTL_FAST_MATH = YES;
 				ONLY_ACTIVE_ARCH = YES;
@@ -1468,6 +1470,7 @@
 				CLANG_WARN_UNREACHABLE_CODE = YES;
 				CLANG_WARN__DUPLICATE_METHOD_MATCH = YES;
 				COPY_PHASE_STRIP = NO;
+				CURRENT_PROJECT_VERSION = 2024013101;
 				DEBUG_INFORMATION_FORMAT = "dwarf-with-dsym";
 				ENABLE_NS_ASSERTIONS = NO;
 				ENABLE_STRICT_OBJC_MSGSEND = YES;
@@ -1480,6 +1483,7 @@
 				GCC_WARN_UNUSED_FUNCTION = YES;
 				GCC_WARN_UNUSED_VARIABLE = YES;
 				IPHONEOS_DEPLOYMENT_TARGET = 16.0;
+				MARKETING_VERSION = 1.3.12;
 				MTL_ENABLE_DEBUG_INFO = NO;
 				MTL_FAST_MATH = YES;
 				SDKROOT = iphoneos;
@@ -1499,7 +1503,6 @@
 				ASSETCATALOG_COMPILER_GLOBAL_ACCENT_COLOR_NAME = AccentColor;
 				CODE_SIGN_ENTITLEMENTS = Surround/Surround.entitlements;
 				CODE_SIGN_STYLE = Automatic;
-				CURRENT_PROJECT_VERSION = 2023050701;
 				DEVELOPMENT_ASSET_PATHS = "\"Surround/Preview Content\"";
 				DEVELOPMENT_TEAM = 269H7P4WSN;
 				ENABLE_PREVIEWS = YES;
@@ -1510,7 +1513,6 @@
 					"$(inherited)",
 					"@executable_path/Frameworks",
 				);
-				MARKETING_VERSION = 1.3.11;
 				OTHER_SWIFT_FLAGS = "-D MAIN_APP";
 				PRODUCT_BUNDLE_IDENTIFIER = com.honganhkhoa.Surround;
 				PRODUCT_NAME = "$(TARGET_NAME)";
@@ -1529,7 +1531,6 @@
 				ASSETCATALOG_COMPILER_GLOBAL_ACCENT_COLOR_NAME = AccentColor;
 				CODE_SIGN_ENTITLEMENTS = Surround/Surround.entitlements;
 				CODE_SIGN_STYLE = Automatic;
-				CURRENT_PROJECT_VERSION = 2023050701;
 				DEVELOPMENT_ASSET_PATHS = "\"Surround/Preview Content\"";
 				DEVELOPMENT_TEAM = 269H7P4WSN;
 				ENABLE_PREVIEWS = YES;
@@ -1540,7 +1541,6 @@
 					"$(inherited)",
 					"@executable_path/Frameworks",
 				);
-				MARKETING_VERSION = 1.3.11;
 				OTHER_SWIFT_FLAGS = "-D MAIN_APP";
 				PRODUCT_BUNDLE_IDENTIFIER = com.honganhkhoa.Surround;
 				PRODUCT_NAME = "$(TARGET_NAME)";
@@ -1638,7 +1638,6 @@
 			buildSettings = {
 				CODE_SIGN_ENTITLEMENTS = SurroundNotificationContent/SurroundNotificationContent.entitlements;
 				CODE_SIGN_STYLE = Automatic;
-				CURRENT_PROJECT_VERSION = 2023050701;
 				DEVELOPMENT_TEAM = 269H7P4WSN;
 				INFOPLIST_FILE = SurroundNotificationContent/Info.plist;
 				IPHONEOS_DEPLOYMENT_TARGET = 15.0;
@@ -1647,7 +1646,6 @@
 					"@executable_path/Frameworks",
 					"@executable_path/../../Frameworks",
 				);
-				MARKETING_VERSION = 1.3.11;
 				PRODUCT_BUNDLE_IDENTIFIER = com.honganhkhoa.Surround.SurroundNotificationContent;
 				PRODUCT_NAME = "$(TARGET_NAME)";
 				SKIP_INSTALL = YES;
@@ -1662,7 +1660,6 @@
 			buildSettings = {
 				CODE_SIGN_ENTITLEMENTS = SurroundNotificationContent/SurroundNotificationContent.entitlements;
 				CODE_SIGN_STYLE = Automatic;
-				CURRENT_PROJECT_VERSION = 2023050701;
 				DEVELOPMENT_TEAM = 269H7P4WSN;
 				INFOPLIST_FILE = SurroundNotificationContent/Info.plist;
 				IPHONEOS_DEPLOYMENT_TARGET = 15.0;
@@ -1671,7 +1668,6 @@
 					"@executable_path/Frameworks",
 					"@executable_path/../../Frameworks",
 				);
-				MARKETING_VERSION = 1.3.11;
 				PRODUCT_BUNDLE_IDENTIFIER = com.honganhkhoa.Surround.SurroundNotificationContent;
 				PRODUCT_NAME = "$(TARGET_NAME)";
 				SKIP_INSTALL = YES;
@@ -1688,7 +1684,6 @@
 				ASSETCATALOG_COMPILER_WIDGET_BACKGROUND_COLOR_NAME = WidgetBackground;
 				CODE_SIGN_ENTITLEMENTS = SurroundWidgetsExtension.entitlements;
 				CODE_SIGN_STYLE = Automatic;
-				CURRENT_PROJECT_VERSION = 2023050701;
 				DEVELOPMENT_TEAM = 269H7P4WSN;
 				GCC_PREPROCESSOR_DEFINITIONS = "";
 				INFOPLIST_FILE = SurroundWidgets/Info.plist;
@@ -1698,7 +1693,6 @@
 					"@executable_path/Frameworks",
 					"@executable_path/../../Frameworks",
 				);
-				MARKETING_VERSION = 1.3.11;
 				OTHER_SWIFT_FLAGS = "";
 				PRODUCT_BUNDLE_IDENTIFIER = com.honganhkhoa.Surround.SurroundWidgets;
 				PRODUCT_NAME = "$(TARGET_NAME)";
@@ -1716,7 +1710,6 @@
 				ASSETCATALOG_COMPILER_WIDGET_BACKGROUND_COLOR_NAME = WidgetBackground;
 				CODE_SIGN_ENTITLEMENTS = SurroundWidgetsExtension.entitlements;
 				CODE_SIGN_STYLE = Automatic;
-				CURRENT_PROJECT_VERSION = 2023050701;
 				DEVELOPMENT_TEAM = 269H7P4WSN;
 				GCC_PREPROCESSOR_DEFINITIONS = "";
 				INFOPLIST_FILE = SurroundWidgets/Info.plist;
@@ -1726,7 +1719,6 @@
 					"@executable_path/Frameworks",
 					"@executable_path/../../Frameworks",
 				);
-				MARKETING_VERSION = 1.3.11;
 				OTHER_SWIFT_FLAGS = "";
 				PRODUCT_BUNDLE_IDENTIFIER = com.honganhkhoa.Surround.SurroundWidgets;
 				PRODUCT_NAME = "$(TARGET_NAME)";
@@ -1742,7 +1734,6 @@
 			buildSettings = {
 				CODE_SIGN_ENTITLEMENTS = SurroundNotificationService/SurroundNotificationService.entitlements;
 				CODE_SIGN_STYLE = Automatic;
-				CURRENT_PROJECT_VERSION = 2023050701;
 				DEVELOPMENT_TEAM = 269H7P4WSN;
 				INFOPLIST_FILE = SurroundNotificationService/Info.plist;
 				IPHONEOS_DEPLOYMENT_TARGET = 15.0;
@@ -1751,7 +1742,6 @@
 					"@executable_path/Frameworks",
 					"@executable_path/../../Frameworks",
 				);
-				MARKETING_VERSION = 1.3.11;
 				PRODUCT_BUNDLE_IDENTIFIER = com.honganhkhoa.Surround.SurroundNotificationService;
 				PRODUCT_NAME = "$(TARGET_NAME)";
 				SKIP_INSTALL = YES;
@@ -1766,7 +1756,6 @@
 			buildSettings = {
 				CODE_SIGN_ENTITLEMENTS = SurroundNotificationService/SurroundNotificationService.entitlements;
 				CODE_SIGN_STYLE = Automatic;
-				CURRENT_PROJECT_VERSION = 2023050701;
 				DEVELOPMENT_TEAM = 269H7P4WSN;
 				INFOPLIST_FILE = SurroundNotificationService/Info.plist;
 				IPHONEOS_DEPLOYMENT_TARGET = 15.0;
@@ -1775,7 +1764,6 @@
 					"@executable_path/Frameworks",
 					"@executable_path/../../Frameworks",
 				);
-				MARKETING_VERSION = 1.3.11;
 				PRODUCT_BUNDLE_IDENTIFIER = com.honganhkhoa.Surround.SurroundNotificationService;
 				PRODUCT_NAME = "$(TARGET_NAME)";
 				SKIP_INSTALL = YES;

Surround/Components/ChallengeCell.swift
@@ -424,7 +424,7 @@ struct ChallengeCell: View {
                 HStack {
                     VStack(alignment: .leading) {
                         if challenge.useCustomKomi {
-                            Label("Custom komi: \(String(format: "%.1f", challenge.game.komi!))", systemImage: "exclamationmark.triangle.fill")
+                            Label("Custom komi: \(challenge.game.komi!, specifier: "%.1f")", systemImage: "exclamationmark.triangle.fill")
                         }
                         if challenge.hasHandicap {
                             Label("Has handicap: \(challenge.game.handicap)", systemImage: "exclamationmark.triangle.fill")
@@ -472,7 +472,7 @@ struct ChallengeCell: View {
                             Spacer()
                             Text("Komi: ").bold()
                                 .offset(x: 8)
-                            Text(String(format: "%.1f", game.komi!))
+                            Text(verbatim: String(format: "%.1f", game.komi!))
                         }
                     }
                 } icon: {

Surround/LaunchScreen.storyboard
@@ -1,8 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<document type="com.apple.InterfaceBuilder3.CocoaTouch.Storyboard.XIB" version="3.0" toolsVersion="17701" targetRuntime="iOS.CocoaTouch" propertyAccessControl="none" useAutolayout="YES" launchScreen="YES" useTraitCollections="YES" useSafeAreas="YES" colorMatched="YES" initialViewController="6f3-JE-sIX">
+<document type="com.apple.InterfaceBuilder3.CocoaTouch.Storyboard.XIB" version="3.0" toolsVersion="22505" targetRuntime="iOS.CocoaTouch" propertyAccessControl="none" useAutolayout="YES" launchScreen="YES" useTraitCollections="YES" useSafeAreas="YES" colorMatched="YES" initialViewController="6f3-JE-sIX">
     <device id="retina6_1" orientation="portrait" appearance="light"/>
     <dependencies>
-        <plugIn identifier="com.apple.InterfaceBuilder.IBCocoaTouchPlugin" version="17703"/>
+        <deployment identifier="iOS"/>
+        <plugIn identifier="com.apple.InterfaceBuilder.IBCocoaTouchPlugin" version="22504"/>
         <capability name="Safe area layout guides" minToolsVersion="9.0"/>
         <capability name="System colors in document resources" minToolsVersion="11.0"/>
         <capability name="documents saved in the Xcode 8 format" minToolsVersion="8.0"/>
@@ -13,7 +14,7 @@
             <objects>
                 <navigationController id="6f3-JE-sIX" sceneMemberID="viewController">
                     <navigationBar key="navigationBar" contentMode="scaleToFill" insetsLayoutMarginsFromSafeArea="NO" largeTitles="YES" id="uYR-RO-dli">
-                        <rect key="frame" x="0.0" y="44" width="414" height="96"/>
+                        <rect key="frame" x="0.0" y="48" width="414" height="96"/>
                         <autoresizingMask key="autoresizingMask"/>
                     </navigationBar>
                     <connections>
@@ -24,7 +25,7 @@
             </objects>
             <point key="canvasLocation" x="-654" y="481"/>
         </scene>
-        <!--Active games-->
+        <!--View Controller-->
         <scene sceneID="ovV-WJ-oLc">
             <objects>
                 <viewController id="aQi-ja-0J6" sceneMemberID="viewController">
@@ -34,7 +35,7 @@
                         <viewLayoutGuide key="safeArea" id="1lG-ix-Q5h"/>
                         <color key="backgroundColor" systemColor="systemBackgroundColor"/>
                     </view>
-                    <navigationItem key="navigationItem" title="Active games" id="6ub-n2-JpZ">
+                    <navigationItem key="navigationItem" id="6ub-n2-JpZ">
                         <barButtonItem key="leftBarButtonItem" image="house" catalog="system" id="MfF-wu-QnU"/>
                     </navigationItem>
                 </viewController>
@@ -45,7 +46,7 @@
     </scenes>
     <color key="tintColor" name="AccentColor"/>
     <resources>
-        <image name="house" catalog="system" width="128" height="106"/>
+        <image name="house" catalog="system" width="128" height="104"/>
         <namedColor name="AccentColor">
             <color red="0.3449999988079071" green="0.33700001239776611" blue="0.83899998664855957" alpha="1" colorSpace="custom" customColorSpace="sRGB"/>
         </namedColor>

Surround/LaunchScreen~iPad.storyboard
@@ -1,8 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<document type="com.apple.InterfaceBuilder3.CocoaTouch.Storyboard.XIB" version="3.0" toolsVersion="17701" targetRuntime="iOS.CocoaTouch" propertyAccessControl="none" useAutolayout="YES" launchScreen="YES" useTraitCollections="YES" useSafeAreas="YES" colorMatched="YES" initialViewController="JvB-BH-gxO">
+<document type="com.apple.InterfaceBuilder3.CocoaTouch.Storyboard.XIB" version="3.0" toolsVersion="22505" targetRuntime="iOS.CocoaTouch" propertyAccessControl="none" useAutolayout="YES" launchScreen="YES" useTraitCollections="YES" useSafeAreas="YES" colorMatched="YES" initialViewController="JvB-BH-gxO">
     <device id="ipad10_9rounded" orientation="landscape" layout="fullscreen" appearance="light"/>
     <dependencies>
-        <plugIn identifier="com.apple.InterfaceBuilder.IBCocoaTouchPlugin" version="17703"/>
+        <deployment identifier="iOS"/>
+        <plugIn identifier="com.apple.InterfaceBuilder.IBCocoaTouchPlugin" version="22504"/>
         <capability name="Safe area layout guides" minToolsVersion="9.0"/>
         <capability name="System colors in document resources" minToolsVersion="11.0"/>
         <capability name="documents saved in the Xcode 8 format" minToolsVersion="8.0"/>
@@ -13,7 +14,7 @@
             <objects>
                 <navigationController id="tgW-vv-0gk" sceneMemberID="viewController">
                     <navigationBar key="navigationBar" contentMode="scaleToFill" insetsLayoutMarginsFromSafeArea="NO" largeTitles="YES" id="9FU-Jw-4mc">
-                        <rect key="frame" x="0.0" y="0.0" width="420" height="102"/>
+                        <rect key="frame" x="0.0" y="24" width="420" height="102"/>
                         <autoresizingMask key="autoresizingMask"/>
                     </navigationBar>
                     <connections>
@@ -29,7 +30,7 @@
             <objects>
                 <navigationController id="zu1-Gv-QyI" sceneMemberID="viewController">
                     <navigationBar key="navigationBar" contentMode="scaleToFill" insetsLayoutMarginsFromSafeArea="NO" largeTitles="YES" id="PtS-ym-jjo">
-                        <rect key="frame" x="0.0" y="0.0" width="859.5" height="102"/>
+                        <rect key="frame" x="0.0" y="24" width="859.5" height="102"/>
                         <autoresizingMask key="autoresizingMask"/>
                     </navigationBar>
                     <connections>
@@ -56,7 +57,7 @@
             </objects>
             <point key="canvasLocation" x="1146" y="13"/>
         </scene>
-        <!--Active games-->
+        <!--View Controller-->
         <scene sceneID="mNW-ox-DmE">
             <objects>
                 <viewController id="uko-jv-xkd" sceneMemberID="viewController">
@@ -66,7 +67,7 @@
                         <viewLayoutGuide key="safeArea" id="5OO-YW-YPs"/>
                         <color key="backgroundColor" systemColor="systemBackgroundColor"/>
                     </view>
-                    <navigationItem key="navigationItem" title="Active games" id="Fwh-ZL-7Z7"/>
+                    <navigationItem key="navigationItem" id="Fwh-ZL-7Z7"/>
                 </viewController>
                 <placeholder placeholderIdentifier="IBFirstResponder" id="G3E-qA-ecR" userLabel="First Responder" customClass="UIResponder" sceneMemberID="firstResponder"/>
             </objects>

Surround/Localizable.xcstrings
@@ -414,6 +414,22 @@
         }
       }
     },
+    "%.1f komi" : {
+      "localizations" : {
+        "ja" : {
+          "stringUnit" : {
+            "state" : "translated",
+            "value" : "%.1f目のコミ"
+          }
+        },
+        "vi" : {
+          "stringUnit" : {
+            "state" : "translated",
+            "value" : "%.1f komi"
+          }
+        }
+      }
+    },
     "%@ %@" : {
       "comment" : "Time left on timer - more than 2 days [days - hours]",
       "localizations" : {
@@ -461,22 +477,6 @@
           "stringUnit" : {
             "state" : "translated",
             "value" : "%1$@ + %2$@ cho tới %3$@"
-          }
-        }
-      }
-    },
-    "%@ komi" : {
-      "localizations" : {
-        "ja" : {
-          "stringUnit" : {
-            "state" : "translated",
-            "value" : "%@目のコミ"
-          }
-        },
-        "vi" : {
-          "stringUnit" : {
-            "state" : "translated",
-            "value" : "%@ komi"
           }
         }
       }
@@ -560,7 +560,7 @@
       }
     },
     "%lld captures" : {
-      "comment" : "SingleGameView - vary for plural\nshow captures in player banner view",
+      "comment" : "show captures in player banner view\nSingleGameView - vary for plural",
       "localizations" : {
         "en" : {
           "variations" : {
@@ -1741,6 +1741,16 @@
         }
       }
     },
+    "Black by %.1f" : {
+      "localizations" : {
+        "vi" : {
+          "stringUnit" : {
+            "state" : "translated",
+            "value" : "Đen hơn %.1f"
+          }
+        }
+      }
+    },
     "Black to move" : {
       "localizations" : {
         "ja" : {
@@ -2481,12 +2491,12 @@
         }
       }
     },
-    "Custom komi: %@" : {
+    "Custom komi: %.1f" : {
       "localizations" : {
         "vi" : {
           "stringUnit" : {
             "state" : "translated",
-            "value" : "Komi tự chọn: %@"
+            "value" : "Komi tự chọn: %.1f"
           }
         }
       }
@@ -5658,6 +5668,16 @@
         }
       }
     },
+    "White by %.1f" : {
+      "localizations" : {
+        "vi" : {
+          "stringUnit" : {
+            "state" : "translated",
+            "value" : "Trắng hơn %.1f"
+          }
+        }
+      }
+    },
     "White to move" : {
       "localizations" : {
         "ja" : {
@@ -5911,19 +5931,19 @@
         }
       }
     },
-    "Your move (%@)" : {
+    "Your move (%02d)" : {
       "comment" : "Integer is for time left",
       "localizations" : {
         "ja" : {
           "stringUnit" : {
             "state" : "translated",
-            "value" : "あなたの番（%@）"
+            "value" : "あなたの番（%02d）"
           }
         },
         "vi" : {
           "stringUnit" : {
             "state" : "translated",
-            "value" : "Lượt của bạn (%@)"
+            "value" : "Lượt của bạn (%02d)"
           }
         }
       }

Surround/Views/GameDetail/PlayersBannerView.swift
@@ -180,7 +180,7 @@ struct PlayersBannerView: View {
                         .font(Font.caption.monospacedDigit())
                     if let komi = game.gameData?.komi {
                         if color == .white && komi != 0 {
-                            Text("\(String(format: "%.1f", komi)) komi")
+                            Text("\(komi, specifier: "%.1f") komi")
                                 .font(Font.caption.monospacedDigit())
                         }
                     }

commit c76cf7e8222244a9332410ff9f20ef1855f0f462

[Misc] Fix wrong estimation difference caused by 816c6a1.

Surround/Models/Core/Game.swift
@@ -671,7 +671,7 @@ class Game: ObservableObject, Identifiable, CustomDebugStringConvertible, Equata
             if difference > 0 {
                 return String(localized: "White by \(difference, specifier: "%.1f")")
             } else {
-                return String(localized: "Black by \(difference, specifier: "%.1f")")
+                return String(localized: "Black by \(-difference, specifier: "%.1f")")
             }
         } else {
             if gamePhase == .stoneRemoval {